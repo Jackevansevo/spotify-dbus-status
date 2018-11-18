3379b504330088a9040d26840e80553bc37c6c76
Jack Evans <jack@evans.gb.net>
2018-11-18 15:21:30 +0000

    Format code with black

--- setup.py
+++ setup.py
@@ -4,27 +4,18 @@ setup(
     name="spotify-dbus-status",
     version="0.1.3",
     url="https://github.com/Jackevansevo/spotify-dbus-status",
-
     author="Jack Evans",
     author_email="jack@evans.gb.net",
-
     description="Get the current spotify status from cmd line",
-    long_description=open('README.rst').read(),
-
-    license='MIT',
-
-    py_modules=['spotify_dbus_status'],
-    entry_points={
-        'console_scripts': [
-            'spotify-dbus-status=spotify_dbus_status:main'
-        ]
-    },
-
+    long_description=open("README.rst").read(),
+    license="MIT",
+    py_modules=["spotify_dbus_status"],
+    entry_points={"console_scripts": ["spotify-dbus-status=spotify_dbus_status:main"]},
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
 )
--- spotify_dbus_status.py
+++ spotify_dbus_status.py
@@ -15,19 +15,19 @@ metadata = interface.Get("org.mpris.MediaPlayer2.Player", "Metadata")
 
 def main():
 
-    formats = ('json', 'str')
+    formats = ("json", "str")
     parser = ArgumentParser()
-    parser.add_argument('--all', action='store_true')
-    parser.add_argument('--artist', action='store_true')
-    parser.add_argument('--song', action='store_true')
-    parser.add_argument('--album', action='store_true')
-    parser.add_argument('--format', default='str', choices=formats)
-    parser.add_argument('--sep', default=' - ')
-    parser.add_argument('--template')
+    parser.add_argument("--all", action="store_true")
+    parser.add_argument("--artist", action="store_true")
+    parser.add_argument("--song", action="store_true")
+    parser.add_argument("--album", action="store_true")
+    parser.add_argument("--format", default="str", choices=formats)
+    parser.add_argument("--sep", default=" - ")
+    parser.add_argument("--template")
 
     args = parser.parse_args()
 
-    data_flags = ('artist', 'song', 'album')
+    data_flags = ("artist", "song", "album")
 
     opts = dict()
 
@@ -52,28 +52,26 @@ def main():
         template_strings = (flag for flag in data_flags if getattr(args, flag))
 
         # Create a template string "{artist} {song} ..."
-        args.template = args.sep.join([
-            '{' + flag + '}' for flag in template_strings
-        ])
+        args.template = args.sep.join(["{" + flag + "}" for flag in template_strings])
 
     data = dict()
 
     if args.artist:
-        data['artist'] = next(iter(metadata.get('xesam:albumArtist'))).encode('utf-8')
+        data["artist"] = next(iter(metadata.get("xesam:albumArtist"))).encode("utf-8")
 
     if args.song:
-        data['song'] = metadata.get('xesam:title').encode('utf-8')
+        data["song"] = metadata.get("xesam:title").encode("utf-8")
 
     if args.album:
-        data['album'] = metadata.get('xesam:album').encode('utf-8')
+        data["album"] = metadata.get("xesam:album").encode("utf-8")
 
     formatters = {
-        'json': lambda d: stdout.write(json.dumps(d)),
-        'str': lambda d: stdout.write(args.template.format(**data)),
+        "json": lambda d: stdout.write(json.dumps(d)),
+        "str": lambda d: stdout.write(args.template.format(**data)),
     }
 
     formatters.get(args.format)(data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
